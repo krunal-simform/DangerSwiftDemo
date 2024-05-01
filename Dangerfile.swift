103dbf72ff5915889bd0be1acf8b7ba5edb78b48
Krunal Patel <krunal.patel@simformsolutions.com>
2024-05-01 12:19:19 +0530

    Update workflow

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -9,10 +9,13 @@ if editedFiles.count - danger.git.deletedFiles.count > 300 {
 }
 
 // Encourage writing up some reasoning about the PR, rather than just leaving a title.
-let body = danger.github.pullRequest.body?.count ?? 0
-let linesOfCode = danger.github.pullRequest.additions ?? 0
-if body < 3 && linesOfCode > 10 {
-    warn("Please provide a summary in the Pull Request description")
+if let body = danger.github.pullRequest.body?.lowercased() {
+    let issuePattern = try! Regex("#[0-9]+")
+    if !body.contains(issuePattern) {
+        warn("Please add issue id.")
+    }
+} else {
+    warn("Please add issue id.")
 }
 
 // Support running via `danger local`
@@ -24,4 +27,10 @@ if danger.github != nil {
 }
 
 print("Running Swiftlint on changed files...")
+<<<<<<< Updated upstream
 SwiftLint.lint(.files(editedFiles), inline: true, strict: true, quiet: false)
+message("Done")
+=======
+SwiftLint.lint(.files(editedFiles), inline: true, strict: true, quiet: false, configFile)
+SwiftLint.lint(.files(editedFiles), inline: true, configFile: ".swiftlint.yml", strict: true, quiet: false)
+>>>>>>> Stashed changes
